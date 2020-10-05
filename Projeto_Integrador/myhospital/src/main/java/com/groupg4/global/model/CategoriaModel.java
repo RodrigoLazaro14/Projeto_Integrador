5d606f2a77e95ff54339f70d8cc5998c14b59cce
unknown <lazaro_db@hotmail.com>
2020-10-05 12:15:38 -0300

    Models do projeto integrador

--- Projeto_Integrador/myhospital/src/main/java/com/groupg4/global/model/CategoriaModel.java
+++ Projeto_Integrador/myhospital/src/main/java/com/groupg4/global/model/CategoriaModel.java
@@ -2,107 +2,116 @@ package com.groupg4.global.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Entity
+@Table(name="tb_categoria")
 public class CategoriaModel {
 	
-	@Entity
-	@Table(name="tb_categoria")
-	public class ServicoModel 
-	{
-		@Column
-		private boolean clinico_geral;
-		@Column
-		private double preco_clinico_geral;
-		@Column
-		private boolean cirugiao_geral;
-		@Column
-		private double preco_cirugiao_geral;
-		@Column
-		private boolean endocrinologia;
-		@Column
-		private double preco_endocrinologia;
-		@Column
-		private boolean ginecolociaObstetricia;
-		@Column
-		private double preco_ginecolociaObstetricia;
-		private boolean neurologia;
-		@Column
-		private double preco_neurologia;
-		@Column
-		private Long fk_id_hospital;
-		
-		//Getters and Setters
-		
-		public boolean isClinico_geral() {
-			return clinico_geral;
-		}
-		public void setClinico_geral(boolean clinico_geral) {
-			this.clinico_geral = clinico_geral;
-		}
-		public double getPreco_clinico_geral() {
-			return preco_clinico_geral;
-		}
-		public void setPreco_clinico_geral(double preco_clinico_geral) {
-			this.preco_clinico_geral = preco_clinico_geral;
-		}
-		public boolean isCirugiao_geral() {
-			return cirugiao_geral;
-		}
-		public void setCirugiao_geral(boolean cirugiao_geral) {
-			this.cirugiao_geral = cirugiao_geral;
-		}
-		public double getPreco_cirugiao_geral() {
-			return preco_cirugiao_geral;
-		}
-		public void setPreco_cirugiao_geral(double preco_cirugiao_geral) {
-			this.preco_cirugiao_geral = preco_cirugiao_geral;
-		}
-		public boolean isEndocrinologia() {
-			return endocrinologia;
-		}
-		public void setEndocrinologia(boolean endocrinologia) {
-			this.endocrinologia = endocrinologia;
-		}
-		public double getPreco_endocrinologia() {
-			return preco_endocrinologia;
-		}
-		public void setPreco_endocrinologia(double preco_endocrinologia) {
-			this.preco_endocrinologia = preco_endocrinologia;
-		}
-		public boolean isGinecolociaObstetricia() {
-			return ginecolociaObstetricia;
-		}
-		public void setGinecolociaObstetricia(boolean ginecolociaObstetricia) {
-			this.ginecolociaObstetricia = ginecolociaObstetricia;
-		}
-		public double getPreco_ginecolociaObstetricia() {
-			return preco_ginecolociaObstetricia;
-		}
-		public void setPreco_ginecolociaObstetricia(double preco_ginecolociaObstetricia) {
-			this.preco_ginecolociaObstetricia = preco_ginecolociaObstetricia;
-		}
-		public boolean isNeurologia() {
-			return neurologia;
-		}
-		public void setNeurologia(boolean neurologia) {
-			this.neurologia = neurologia;
-		}
-		public double getPreco_neurologia() {
-			return preco_neurologia;
-		}
-		public void setPreco_neurologia(double preco_neurologia) {
-			this.preco_neurologia = preco_neurologia;
-		}
-		public Long getFk_id_hospital() {
-			return fk_id_hospital;
-		}
-		public void setFk_id_hospital(Long fk_id_hospital) {
-			this.fk_id_hospital = fk_id_hospital;
-		}
-		
+	@Id
+	@Column
+	@GeneratedValue(strategy=GenerationType.SEQUENCE) //Faz com que o ID seja auto increment, e também já indica que é uma chave primaria (PRIMARY KEY).
+	private Long id_categoria;
+	@Column
+	private boolean clinico_geral;
+	@Column
+	private double preco_clinico_geral;
+	@Column
+	private boolean cirugiao_geral;
+	@Column
+	private double preco_cirugiao_geral;
+	@Column
+	private boolean endocrinologia;
+	@Column
+	private double preco_endocrinologia;
+	@Column
+	private boolean ginecolociaObstetricia;
+	@Column
+	private double preco_ginecolociaObstetricia;
+	@Column
+	private boolean neurologia;
+	@Column
+	private double preco_neurologia;
+	@Column
+	private Long fk_id_hospital;
+	
+	//Getters and Setters
+	
+	public boolean isClinico_geral() {
+		return clinico_geral;
 	}
-	
-	
+	public void setClinico_geral(boolean clinico_geral) {
+		this.clinico_geral = clinico_geral;
+	}
+	public double getPreco_clinico_geral() {
+		return preco_clinico_geral;
+	}
+	public void setPreco_clinico_geral(double preco_clinico_geral) {
+		this.preco_clinico_geral = preco_clinico_geral;
+	}
+	public boolean isCirugiao_geral() {
+		return cirugiao_geral;
+	}
+	public void setCirugiao_geral(boolean cirugiao_geral) {
+		this.cirugiao_geral = cirugiao_geral;
+	}
+	public double getPreco_cirugiao_geral() {
+		return preco_cirugiao_geral;
+	}
+	public void setPreco_cirugiao_geral(double preco_cirugiao_geral) {
+		this.preco_cirugiao_geral = preco_cirugiao_geral;
+	}
+	public boolean isEndocrinologia() {
+		return endocrinologia;
+	}
+	public void setEndocrinologia(boolean endocrinologia) {
+		this.endocrinologia = endocrinologia;
+	}
+	public double getPreco_endocrinologia() {
+		return preco_endocrinologia;
+	}
+	public void setPreco_endocrinologia(double preco_endocrinologia) {
+		this.preco_endocrinologia = preco_endocrinologia;
+	}
+	public boolean isGinecolociaObstetricia() {
+		return ginecolociaObstetricia;
+	}
+	public void setGinecolociaObstetricia(boolean ginecolociaObstetricia) {
+		this.ginecolociaObstetricia = ginecolociaObstetricia;
+	}
+	public double getPreco_ginecolociaObstetricia() {
+		return preco_ginecolociaObstetricia;
+	}
+	public void setPreco_ginecolociaObstetricia(double preco_ginecolociaObstetricia) {
+		this.preco_ginecolociaObstetricia = preco_ginecolociaObstetricia;
+	}
+	public boolean isNeurologia() {
+		return neurologia;
+	}
+	public void setNeurologia(boolean neurologia) {
+		this.neurologia = neurologia;
+	}
+	public double getPreco_neurologia() {
+		return preco_neurologia;
+	}
+	public void setPreco_neurologia(double preco_neurologia) {
+		this.preco_neurologia = preco_neurologia;
+	}
+	public Long getFk_id_hospital() {
+		return fk_id_hospital;
+	}
+	public void setFk_id_hospital(Long fk_id_hospital) {
+		this.fk_id_hospital = fk_id_hospital;
+	}
+	public Long getId_categoria() {
+		return id_categoria;
+	}
+	public void setId_categoria(Long id_categoria) {
+		this.id_categoria = id_categoria;
+	}
+		
 
 }
--- Projeto_Integrador/myhospital/src/main/java/com/groupg4/global/model/FuncionarioModel.java
+++ Projeto_Integrador/myhospital/src/main/java/com/groupg4/global/model/FuncionarioModel.java
@@ -6,59 +6,54 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+@Entity
+@Table(name="tb_funcionario")
 public class FuncionarioModel {
 	
-	@Entity
-	@Table(name="tb_funcionario")
-	public class ServicoModel 
-	{
-		@Id
-		@Column
-		@GeneratedValue(strategy=GenerationType.SEQUENCE) //Faz com que o ID seja auto increment, e também já indica que é uma chave primaria (PRIMARY KEY).
-		private Long id_funcionario;
-		@Column
-		private String nome_funcionario;
-		@Column
-		private String senha_funcionario;
-		@Column
-		private String email_funcionario;
-		@Column
-		private String codigo_hospital_funcionario;
-		
-		//Getters and Setters
-		
-		public Long getId_funcionario() {
-			return id_funcionario;
-		}
-		public void setId_funcionario(Long id_funcionario) {
-			this.id_funcionario = id_funcionario;
-		}
-		public String getNome_funcionario() {
-			return nome_funcionario;
-		}
-		public void setNome_funcionario(String nome_funcionario) {
-			this.nome_funcionario = nome_funcionario;
-		}
-		public String getSenha_funcionario() {
-			return senha_funcionario;
-		}
-		public void setSenha_funcionario(String senha_funcionario) {
-			this.senha_funcionario = senha_funcionario;
-		}
-		public String getEmail_funcionario() {
-			return email_funcionario;
-		}
-		public void setEmail_funcionario(String email_funcionario) {
-			this.email_funcionario = email_funcionario;
-		}
-		public String getCodigo_hospital_funcionario() {
-			return codigo_hospital_funcionario;
-		}
-		public void setCodigo_hospital_funcionario(String codigo_hospital_funcionario) {
-			this.codigo_hospital_funcionario = codigo_hospital_funcionario;
-		}
-
+	@Id
+	@Column
+	@GeneratedValue(strategy=GenerationType.SEQUENCE) //Faz com que o ID seja auto increment, e também já indica que é uma chave primaria (PRIMARY KEY).
+	private Long id_funcionario;
+	@Column
+	private String nome_funcionario;
+	@Column
+	private String senha_funcionario;
+	@Column
+	private String email_funcionario;
+	@Column
+	private String codigo_hospital_funcionario;
+	
+	//Getters and Setters
+	
+	public Long getId_funcionario() {
+		return id_funcionario;
+	}
+	public void setId_funcionario(Long id_funcionario) {
+		this.id_funcionario = id_funcionario;
+	}
+	public String getNome_funcionario() {
+		return nome_funcionario;
+	}
+	public void setNome_funcionario(String nome_funcionario) {
+		this.nome_funcionario = nome_funcionario;
+	}
+	public String getSenha_funcionario() {
+		return senha_funcionario;
+	}
+	public void setSenha_funcionario(String senha_funcionario) {
+		this.senha_funcionario = senha_funcionario;
+	}
+	public String getEmail_funcionario() {
+		return email_funcionario;
+	}
+	public void setEmail_funcionario(String email_funcionario) {
+		this.email_funcionario = email_funcionario;
+	}
+	public String getCodigo_hospital_funcionario() {
+		return codigo_hospital_funcionario;
+	}
+	public void setCodigo_hospital_funcionario(String codigo_hospital_funcionario) {
+		this.codigo_hospital_funcionario = codigo_hospital_funcionario;
 	}
 
 }
