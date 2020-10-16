abb81e6e17c849554be012aba7efbe366c356cc9
unknown <leandroefernandapaiva@gmail.com>
2020-10-16 15:31:31 -0300

    16-10

--- Projeto_Integrador/myhospital/src/main/java/com/groupg4/global/controller/FuncionarioController.java
+++ Projeto_Integrador/myhospital/src/main/java/com/groupg4/global/controller/FuncionarioController.java
@@ -1,6 +1,9 @@
 package com.groupg4.global.controller;
 
 import java.util.List;
+
+import org.generation.blogPessoal.model.UserLogin;
+import org.generation.blogPessoal.model.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,36 +17,60 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.groupg4.global.repository.FuncionarioRepository;
+import com.google.common.base.Optional;
+import com.groupg4.global.model.FuncionarioLogin;
 import com.groupg4.global.model.FuncionarioModel;
+
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/funcionarios")
 public class FuncionarioController {
 	@Autowired
 	private FuncionarioRepository repository;
+
+	@Autowired
+	private FuncionarioService funcionarioService;
+	
 	@GetMapping
 	public ResponseEntity<List<FuncionarioModel>> getAll() {
 		return ResponseEntity.ok(repository.findAll());
 	}
+
 	@GetMapping("/{id}")
 	public ResponseEntity<FuncionarioModel> getById(@PathVariable long id) {
 		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
+
 	@GetMapping("/nome/{nome}")
 	public ResponseEntity<List<FuncionarioModel>> getByName(@PathVariable String nome) {
 		return ResponseEntity.ok(repository.findAllByNomeFuncionarioContainingIgnoreCase(nome));
 	}
+
 	@PostMapping
 	public ResponseEntity<FuncionarioModel> post(@RequestBody FuncionarioModel funcionario) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(funcionario));
 	}
+
 	@PutMapping("/{id}")
 	public ResponseEntity<FuncionarioModel> put(@PathVariable long id, @RequestBody FuncionarioModel funcionario) {
-        funcionario.setIdFuncionario(id);
+		funcionario.setIdFuncionario(id);
 		return ResponseEntity.ok(repository.save(funcionario));
 	}
+
 	@DeleteMapping("/{id}")
 	public void delete(@PathVariable long id) {
 		repository.deleteById(id);
 	}
+
+	@PostMapping("/logar")
+	public ResponseEntity<FuncionarioLogin> Autentication(@RequestBody Optional<FuncionarioLogin> user) {
+		return funcionarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+	@PostMapping("/cadastrar")
+	public ResponseEntity<emailFuncionario> Post(@RequestBody emailFuncionario email) {
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(funcionarioService.CadastrarFuncionario(email));
+	}
+
 }
