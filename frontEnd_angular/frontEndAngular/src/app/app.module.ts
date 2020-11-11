12d63289894c2f608d4cee140f762f9e9e0f9e29
Rodrigo Lázaro <70705052+RodrigoLazaro14@users.noreply.github.com>
2020-11-11 12:50:36 -0300

    Update app.module.ts

--- frontEnd_angular/frontEndAngular/src/app/app.module.ts
+++ frontEnd_angular/frontEndAngular/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SobrenosComponent } from './sobrenos/sobrenos.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SobrenosComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
